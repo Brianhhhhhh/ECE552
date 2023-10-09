72f15ac956935a914bea390fb1d7895027b7ab0e
Kkerhialoxy <allen.wwhl@gmail.com>
2023-10-09 16:36:29 -0500

    BranchMux Revise

--- BranchMux.v
+++ BranchMux.v
@@ -9,13 +9,13 @@ module BranchMux(
     // Flag[2] = Z Flag[1] = V Flag[0] = N 
     always @(ccc, Flag) begin
         case (ccc)
-            4'b0000: temp = ~Flag[2];
-            4'b0001: temp = Flag[2];
-            4'b0010: temp = ~(Flag[2] | Flag[0]);
-            4'b0011: temp = Flag[0];
-            4'b0100: temp = Flag[2] || (Flag[2] == 0 && Flag[0] == 0);
-            4'b0101: temp = Flag[2] || Flag[0];
-            4'b0110: temp = Flag[1];
+            3'b000: temp = ~Flag[2];
+            3'b001: temp = Flag[2];
+            3'b010: temp = ~(Flag[2] | Flag[0]);
+            3'b011: temp = Flag[0];
+            3'b100: temp = Flag[2] || (Flag[2] == 0 && Flag[0] == 0);
+            3'b101: temp = Flag[2] || Flag[0];
+            3'b110: temp = Flag[1];
             default: temp = 1;
         endcase
     end

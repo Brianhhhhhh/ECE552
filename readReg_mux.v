cb8b9d86a5c2bb0ef46325ff6fc630d020b2727c
Zequan Tang <tzq1700324992@outlook.com>
2023-10-08 22:42:21 -0500

    Instruction decode part

--- readReg_mux.v
+++ readReg_mux.v
@@ -1,9 +1,11 @@
-module readReg1_mux(selSignal, rs, rd, readReg1);
-	input selSignal;
-	input [3:0] rs, rd;
-	output [3:0] readReg1;
+module readReg1_mux(Opcode, tempo, rd, rs);
+	input [3:0] Opcode;
+	input [3:0] tempo, rd;
+	output [3:0] rs;
 	
-	// if selSignal is 1 (instruction is LLB or LHB), rd will be the input of readReg1
-	assign readReg1 = selSignal ? rd : rs;
-
+	// opcode is LLB or LHB, rd is rs
+	wire select;
+	assign select = (Opcode == 4'b1010) | (Opcode == 4'b1011);
+	assign rs = select ? rd : tempo;
+	
 endmodule

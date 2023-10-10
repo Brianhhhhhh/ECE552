dd1a1834074da40e680b862acc8daeae337bb01a
Kkerhialoxy <allen.wwhl@gmail.com>
2023-10-10 18:33:38 -0500

    flag register

--- FlagRegister.v
+++ FlagRegister.v
@@ -1,12 +1,12 @@
-module flag_register(clk, rst, flag_in, flag_out, en);
+module flag_register(clk, rst, flag_in, flag_out,);
 
 input clk, rst;
-input [2:0] en;
 inout [2:0] flag_in;
 inout [2:0] flag_out;
 
-dff ff0(.q(flag_out[0]), .d(flag_in[0]), .wen(en[0]), .clk(clk), .rst(rst));
-dff ff1(.q(flag_out[1]), .d(flag_in[1]), .wen(en[1]), .clk(clk), .rst(rst));
-dff ff2(.q(flag_out[2]), .d(flag_in[2]), .wen(en[2]), .clk(clk), .rst(rst));
+// always write enable
+dff ff0(.q(flag_out[0]), .d(flag_in[0]), .wen(1'b1), .clk(clk), .rst(rst));
+dff ff1(.q(flag_out[1]), .d(flag_in[1]), .wen(1'b1), .clk(clk), .rst(rst));
+dff ff2(.q(flag_out[2]), .d(flag_in[2]), .wen(1'b1), .clk(clk), .rst(rst));
 
 endmodule

912c6733d5c6865ff3301aedbafd0c7ba7dfd1ac
Kkerhialoxy <allen.wwhl@gmail.com>
2023-10-13 14:49:39 -0500

    branchmux instantiate + targetaddr + pcplus2

--- FlagRegister.v
+++ FlagRegister.v
@@ -5,8 +5,8 @@ inout [2:0] flag_in;
 inout [2:0] flag_out;
 
 // always write enable
-dff ff0(.q(flag_out[0]), .d(flag_in[0]), .wen(1'b1), .clk(clk), .rst(rst));
-dff ff1(.q(flag_out[1]), .d(flag_in[1]), .wen(1'b1), .clk(clk), .rst(rst));
-dff ff2(.q(flag_out[2]), .d(flag_in[2]), .wen(1'b1), .clk(clk), .rst(rst));
+dff i0(.q(flag_out[0]), .d(flag_in[0]), .wen(1'b1), .clk(clk), .rst(rst));
+dff i1(.q(flag_out[1]), .d(flag_in[1]), .wen(1'b1), .clk(clk), .rst(rst));
+dff i2(.q(flag_out[2]), .d(flag_in[2]), .wen(1'b1), .clk(clk), .rst(rst));
 
 endmodule
